793b073105be9119963a2410791f8a1d6310a4af
Heeyeon <go1927hm@gmail.com>
2023-03-26 12:05:52 +0900

    Update Files

--- src/main/java/com/board/interceptor/BoardInterceptor.java
+++ src/main/java/com/board/interceptor/BoardInterceptor.java
@@ -6,24 +6,24 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
-public class BoardInterceptor implements HandlerInterceptor{
-	
-	@Override
+public class BoardInterceptor implements HandlerInterceptor {
+
+    @Override
     public boolean preHandle(HttpServletRequest request
-            , HttpServletResponse response, Object handler) 
-                    throws Exception {
+            , HttpServletResponse response, Object handler)
+            throws Exception {
 
         //세션 객체 생성
-        HttpSession session=request.getSession();
+        HttpSession session = request.getSession();
 
         //세션이 없으면(로그인되지 않은 상태)
-        if(session.getAttribute("member") == null) {
+        if (session.getAttribute("member") == null) {
 
             //login 페이지로 이동
             response.sendRedirect(request.getContextPath()
-                    +"/member/login");
+                    + "/member/login");
             return false; //메인 액션으로 가지 않음
-        }else {
+        } else {
             return true; //메인 액션으로 이동
         }
     }
